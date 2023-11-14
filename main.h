f591c124a3c4bc240548bdbd610bc06b5a753bcd
programmingwithjerry <eneyellowj@gmail.com>
2023-11-14 21:58:34 +0000

    special string test 1

--- main.h
+++ main.h
@@ -1,9 +1,15 @@
 #ifndef MAIN_H
-#define MAIN_H
+
 
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+
+typedef struct _format
+{
+	char type;
+	int (*f)(va_list);
+} format;
 
 int _printf(const char *format, ...);
 int helper(const char *str, va_list my_list);
@@ -12,11 +18,6 @@ int stdo_string(va_list my_list);
 int percent_helper(const char *, va_list, int *);
 int string_length(const char *);
 int stdo_print(char *);
-typedef struct _format
-{
-	char type;
-	int (*f)(va_list);
-} format;
 int _putchar(char c);
 int tmp_store(char c);
 char *itoa(long int num, int base);
@@ -27,4 +28,5 @@ int stdo_octal(va_list my_list);
 int stdo_hexadecimal_upp(va_list my_list);
 int stdo_hexadecimal_low(va_list my_list);
 int stdo_pointer(va_list my_list);
+int tmp_store(char);
 #endif
